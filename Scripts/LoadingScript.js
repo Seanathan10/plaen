849a128ad60443f260dd4983c1cd6f3c43eb1830
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2022-03-16 12:17:25 -0700

    Update LoadingScript.js

--- Scripts/LoadingScript.js
+++ Scripts/LoadingScript.js
@@ -16,8 +16,9 @@ window.onload = function() {
 
 function PlayMTBK(){
     // window.location.href = '../Pages/Game.html';
-    var audio = new Audio("https://audio.jukehost.co.uk/I05zU9wJWYeljIsSMxzyFSm1HqNQRo8G");
-    audio.play();
+    
+    //var audio = new Audio("https://audio.jukehost.co.uk/I05zU9wJWYeljIsSMxzyFSm1HqNQRo8G");
+    //audio.play();
  
     document.getElementById( 'FullDiv' ).style.display = 'block';
     document.getElementById( 'ButtonDiv' ).style.display = 'none';
